509dd6a037b932308c7c97f1cb2f63b45e354067
jinwei <525614992@qq.com>
2018-08-18 23:58:48 +0800

    chore: 普通的 Array 和 ArrayList demo

---
+++ src/main/java/headfirstjava/chap06/ArrayListVsSimpleArray.java
@@ -0,0 +1,32 @@
+package chap06;
+
+import java.util.ArrayList;
+
+class ArrayListVsSimpleArray {
+  private void simpleArrayPrint() {
+    System.out.println("simple array print");
+    String[] myList = new String[2];
+    myList[0] = new String("first line");
+    myList[1] = new String("2nd line");
+    System.out.println("myList length=" + myList.length);
+    System.out.println("for loop a simple array");
+    for (String line : myList) {
+      System.out.println(line);
+    }
+  }
+
+  private void arrayListPrint() {
+    System.out.println("array list print");
+    ArrayList<String> myList = new ArrayList<String>();
+    myList.add(new String("first line"));
+    myList.add(new String("2nd line"));
+    System.out.println("myList.size = " + myList.size());
+    System.out.println("get(0)" + myList.get(0));
+  }
+
+  public static void main(String[] args) {
+    ArrayListVsSimpleArray ins = new ArrayListVsSimpleArray();
+    ins.simpleArrayPrint();
+    ins.arrayListPrint();
+  }
+}
