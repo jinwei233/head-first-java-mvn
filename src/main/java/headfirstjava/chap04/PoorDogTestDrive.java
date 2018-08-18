2e832a91a7e12b87b93d2e8c5646650d91ecdc8f
jinwei <525614992@qq.com>
2018-08-18 22:59:48 +0800

    chore: 实例变量有默认值

--- src/main/java/headfirstjava/chap04/PoorDogTestDrive.java
+++ src/main/java/headfirstjava/chap04/PoorDogTestDrive.java
@@ -3,7 +3,11 @@ package chap04;
 public class PoorDogTestDrive {
   public static void main(String[] args) {
     PoorDog one = new PoorDog();
+    // 实例变量总是有默认值!
+
+    // int 类型的默认值是 0
     System.out.println("Dog size is " + one.getSize());
+    // string 类型的默认值 null
     System.out.println("Dog name is " + one.getName());
   }
 }
